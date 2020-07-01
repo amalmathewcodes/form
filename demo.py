402cae931b5efff565acc3720ea0bde740bbd462
jamshid ct <jamshidct99@gmail.com>
2020-07-01 20:17:28 +0530

    grade updated

--- demo.py
+++ demo.py
@@ -7,8 +7,17 @@ class Result:
     def percent(self):
         percent = (self.pysics + self.maths + self.chemistry ) * 100 / 300
         return percent
+    def check_grade(self,grade):
+        self.grade = grade
+        if grade == "A":
+            print("A grade")
+        elif grade == "B":
+            print("B grade")
+        elif grade == "C":
+            print("C grade")
 
 
 x = Result()
 x.mark(67, 96, 78)
+x.check_grade("A")
 print(x.percent())
